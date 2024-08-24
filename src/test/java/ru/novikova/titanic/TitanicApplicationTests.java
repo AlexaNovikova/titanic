df85b785f690493faa27dd27f9877accafbddc77
alexaNovikova <alexadubinina@mail.ru>
2024-08-24 20:15:19 +0700

    Создан проект

---
+++ src/test/java/ru/novikova/titanic/TitanicApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.novikova.titanic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
